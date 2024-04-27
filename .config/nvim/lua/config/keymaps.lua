5f3db726f11580ea3f35439e7fdc55321bf9d199
AlanMendozax <alanmendoza9801@outlook.com>
2024-04-27 15:32:56 -0600

    debug setup for python added

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
+
+keymap.set("n", '<leader>tc', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_class();
+  end
+end)
+
+keymap.set("n", '<leader>tm', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_method();
+  end
+end)
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
 				"css-lsp",
         "black",
         "mypy",
+        "debugpy"
 			})
 		end,
 	},
