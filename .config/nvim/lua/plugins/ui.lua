845320827ed00d6af909df5bdce12a27b639044e
AlanMendozax <alanmendoza9801@outlook.com>
2024-05-01 18:18:25 -0600

    neovim ui updated

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,4 +1,5 @@
 return {
+
   -- messages, cmdline and the popupmenu
   {
     "folke/noice.nvim",
@@ -87,30 +88,6 @@ return {
         show_close_icon = false,
       },
     },
-  },
-
-  -- filename
-  {
-    "b0o/incline.nvim",
-    event = "BufReadPre",
-    priority = 1200,
-    config = function()
-      require("incline").setup({
-        window = { margin = { vertical = 0, horizontal = 1 } },
-        hide = {
-          cursorline = true,
-        },
-        render = function(props)
-          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+] " .. filename
-          end
-
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
-        end,
-      })
-    end,
   },
 
   {
