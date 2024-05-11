9eb2504e300e991893df0baffc886ebfe6be5da1
AlanMendozax <alanmendoza9801@outlook.com>
2024-05-11 14:43:15 -0600

    Setup for javascript and typescript languages

---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    -- https://github.com/mfussenegger/nvim-dap
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "127.0.0.1",
+        port = 8123,
+        executable = {
+          command = "js-debug-adapter",
+        },
+      }
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+            runtimeExecutable = "node",
+          },
+        }
+      end
+    end,
+  },
+  {
+    -- https://github.com/mfussenegger/nvim-dap-python
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      -- Update the path passed to setup to point to your system or virtual env python binary
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+    end,
+  },
+}
