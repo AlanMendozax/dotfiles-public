cf9fb431fa5a76a2e5fd2b95ee7ed24b53639e0b
Alan Mendoza <alanmendoza9801@outlook.com>
2025-03-01 11:22:42 -0600

    Fix: Fixed colorscheme to feat with Termux env

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,24 @@
 return {
 	{
 		"craftzdog/solarized-osaka.nvim",
-		lazy = true,
+		lazy = false,
 		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
+		opts = {
+			on_colors = function(colors)
+				colors.bg_statusline = "#000000"
+				colors.bg_float = "#000000"
+				colors.bg_sidebar = "#000000"
+			end,
+			on_highlights = function(highlights, colors)
+				highlights.DiagnosticVirtualTextWarn = { fg = colors.yellow500 }
+				highlights.DiagnosticVirtualTextError = { fg = colors.red500 }
+				highlights.DiagnosticVirtualTextHint = { fg = colors.cyan500 }
+				highlights.DiagnosticVirtualTextInfo = { fg = colors.blue500 }
+			end,
+		},
+		config = function(_, opts)
+			require("solarized-osaka").setup(opts)
+			vim.cmd("colorscheme solarized-osaka")
 		end,
 	},
 }
