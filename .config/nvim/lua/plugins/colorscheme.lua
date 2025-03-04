055dd950689db3040a6e45645c16f2d9113b97cf
Alan Mendoza <alanmendoza9801@outlook.com>
2025-03-04 13:43:24 -0600

    Feat: Fixed some colors on neovim and updated tmux statusline

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,15 +5,18 @@ return {
 		priority = 1000,
 		opts = {
 			on_colors = function(colors)
-				colors.bg_statusline = "#000000"
 				colors.bg_float = "#000000"
 				colors.bg_sidebar = "#000000"
+				colors.bg_statusline = "#000000"
 			end,
 			on_highlights = function(highlights, colors)
+				highlights.CursorLine = { bg = "#222222" }
 				highlights.DiagnosticVirtualTextWarn = { fg = colors.yellow500 }
 				highlights.DiagnosticVirtualTextError = { fg = colors.red500 }
 				highlights.DiagnosticVirtualTextHint = { fg = colors.cyan500 }
 				highlights.DiagnosticVirtualTextInfo = { fg = colors.blue500 }
+				highlights.FloatBorder = { fg = "#222222" }
+				highlights.SnacksPickerBorder = { fg = "#222222" }
 			end,
 		},
 		config = function(_, opts)
