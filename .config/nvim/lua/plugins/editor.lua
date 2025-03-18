2806995f4b33d7b134a9378116f9cbc80ea74d99
Alan Mendoza <alanmendoza9801@outlook.com>
2025-03-18 14:15:42 -0600

    Feat: New lazyvim setup for mdx files and blink.cmp plugin

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -81,6 +81,11 @@ return {
 	{
 		"saghen/blink.cmp",
 		opts = {
+			-- completion = {
+			-- 	menu = {
+			-- 		winblend = vim.o.pumblend,
+			-- 	},
+			-- },
 			signature = {
 				window = {
 					winblend = vim.o.pumblend,
