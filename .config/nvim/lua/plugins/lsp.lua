c01eaa66e74c8235363064d1bdf789bbdb0fdef7
Alan Mendoza <alanmendoza9801@outlook.com>
2026-02-07 23:01:25 +0000

    Se agrega configuracion de LSP para neovim

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,97 @@
+return {
+	-- tools
+	{
+		"mason-org/mason.nvim",
+		opts = function(_, opts)
+			local ui_config = require("plugins.ui")
+			opts.ui = ui_config.ui
+			vim.list_extend(opts.ensure_installed, {
+				"stylua",
+				"selene",
+				"shellcheck",
+				"shfmt",
+			})
+		end,
+	},
+
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = false },
+			---@type lspconfig.options
+			servers = {
+				zk = {
+					mason = false,
+					single_file_support = true,
+				},
+				lua_ls = {
+					-- enabled = false,
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								workspaceWord = true,
+								callSnippet = "Both",
+							},
+							misc = {
+								parameters = {
+									-- "--log-level=trace",
+								},
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							type = {
+								castNumberToInteger = true,
+							},
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								-- enable = false,
+								groupSeverity = {
+									strong = "Warning",
+									strict = "Warning",
+								},
+								groupFileStatus = {
+									["ambiguity"] = "Opened",
+									["await"] = "Opened",
+									["codestyle"] = "None",
+									["duplicate"] = "Opened",
+									["global"] = "Opened",
+									["luadoc"] = "Opened",
+									["redefined"] = "Opened",
+									["strict"] = "Opened",
+									["strong"] = "Opened",
+									["type-check"] = "Opened",
+									["unbalanced"] = "Opened",
+									["unused"] = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = {
+								enable = false,
+								defaultConfig = {
+									indent_style = "space",
+									indent_size = "2",
+									continuation_indent_size = "2",
+								},
+							},
+						},
+					},
+				},
+			},
+			setup = {},
+		},
+	},
+}
